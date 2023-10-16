7f7d5f15402430165aab2fd5fdce231ab223cfa6
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2023-10-16 19:22:30 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let button = document.querySelector(".counter");
+let counterNomber = document.querySelector(".nomber");
+let counter = 0;
+button.addEventListener("click", function () {
+  counter = counter + 1;
+  counterNomber.innerText = counter;
+});
+
+let btn = document.querySelector(".reset");
+btn.addEventListener("click", function () {
+  reset = 0;
+  counterNomber.innerHTML = reset;
+});
